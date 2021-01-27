b655d09f4c8408bd6b089d0242afc0e4c2635ccc
David Dong <daviddong201712@gmail.com>
2021-01-27 12:44:05 -0600

    6

--- src/index.js
+++ src/index.js
@@ -150,3 +150,38 @@ function collectStar(player, star) {
     }
 }
 
+function hitBomb(player, bomb) {
+    this.physics.pause();
+
+    player.setTint(0xff0000);
+
+    player.anims.play('turn');
+
+    gameOver = true;
+}
+
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: MyGame
+};
+
+var player;
+var stars;
+var bombs;
+var platforms;
+var cursors;
+var score = 0;
+var gameOver = false;
+var scoreText;
+
+var game = new Phaser.Game(config);
+
